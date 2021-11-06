356c207e04d706f43d7dceecbd0020e2caf1e731
lsm-5 <lsm-5>
2021-11-05 23:56:08 -0300

    task 01

---
+++ task01/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+
+
+public class Main {
+    public static class Point {
+        private int x;
+        private int y;
+
+        public Point(int x, int y){
+            if (x >= 0  && x <=400 && y >= 0 && y<=400) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public int getX() { return x; }
+        public int getY() { return y; }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public void moveBy(int dx, int dy) {
+            setX(getX() + dx);
+            setY(getY() + dy);
+        }
+    }
+
+    public static void main(String[] args) {
+	    Point point = new Point(10, 10);
+        System.out.println(point.x);
+    }
+}
